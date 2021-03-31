415f1458b62deb0b871fee29f0f8714f5779f8c5
nvdinh185 <nvdinh0766777123@gmail.com>
2021-03-31 09:14:59 +0700

    create db mongodb

---
+++ cfg-sample/mongo-69-4.4.0-cty3.js
@@ -0,0 +1,18 @@
+module.exports = Object.freeze({
+    type: "mongodb", //  "mongodb" | "oracle" | "sqlite3"
+    auto_increment_support: false,
+    databaseName: "lucky-draws",
+    serverCfg: {
+        hosts: [{ host: "localhost", port: 27017 }],
+        username: "dinh",
+        password: "123456",
+    },
+    options: {
+        // phần giành cho mongodb thêm
+        //   repSet: "rs0", // Khai báo bộ db replicate
+        isRoot: true, // nếu user của mongo có quyền root
+    },
+    // tham số phụ thêm vào để xác định csdl có hỗ trợ tự tạo auto_increment không?
+    // nếu csdl nào không hổ trợ thì tắt nó đi và sử dụng mô hình model để tạo id tự động
+    // isDebug: true,
+});
