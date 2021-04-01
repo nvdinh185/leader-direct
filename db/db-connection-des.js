492ef036e91fafea0335098e2b94d34b16d6d4d8
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-01 17:04:56 +0700

    innovations

--- db/db-connection-des.js
+++ db/db-connection-des.js
@@ -4,4 +4,4 @@
  */
 
 // khai báo file cấu hình trước khi dùng
-module.exports = require("./db-connection-any")("mongodb", "lucky-draws");
+module.exports = require("./db-connection-any")("sqlite3", "innovations");
