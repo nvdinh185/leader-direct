9eaf367aea509eab97cecb275ca836aa67d58eda
Hieu <hieu2906090@gmail.com>
2021-06-20 11:50:26 +0700

    rmv unneccs translate

--- client-react-iso/src/config/site.config.js
+++ client-react-iso/src/config/site.config.js
@@ -1,9 +1,9 @@
 export default {
-  siteName: "ISOMORPHIC",
+  siteName: "Chỉ Đạo Lãnh Đạo",
   siteIcon: "ion-flash",
-  footerText: `Isomorphic @ ${new Date().getFullYear()} Created by RedQ, Inc`,
+  footerText: `CopyRight Mobifone3@ ${new Date().getFullYear()} Xây dựng bởi P.DVKT`,
   enableAnimatedRoute: false,
-  apiUrl: "http://yoursite.com/api/",
+  apiUrl: "http://10.16.150.69:9232/leader-direct/api/",
   google: {
     analyticsKey: "UA-xxxxxxxxx-1",
   },
--- client-react-iso/src/config/translation/index.js
+++ client-react-iso/src/config/translation/index.js
@@ -1,17 +1,7 @@
-import Enlang from './entries/en-US';
-import Zhlang from './entries/zh-Hans-CN';
-import Salang from './entries/ar_SA';
-import Itlang from './entries/it_IT';
-import Eslang from './entries/es_ES';
-import Frlang from './entries/fr_FR';
+import Enlang from "./entries/en-US";
 
 const AppLocale = {
   en: Enlang,
-  zh: Zhlang,
-  sa: Salang,
-  it: Itlang,
-  es: Eslang,
-  fr: Frlang,
 };
 
 export default AppLocale;
