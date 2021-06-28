d476af170166c3a1d32864dd4ef77b16d2fec9e8
Hieu <hieu2906090@gmail.com>
2021-06-28 17:00:59 +0700

    add edit admin menu

--- client-react-iso/src/constants/common.js
+++ client-react-iso/src/constants/common.js
@@ -1,3 +1,3 @@
-export const NO_TOOLTIP_COLS = ["id", "username", "name", "status", "function_groups", "page"];
+export const NO_TOOLTIP_COLS = ["id", "username", "name", "status", "function_groups", "page", "tag_id", "is_active"];
 export const STR_EDITABLE_COLS = ["name", "page"];
 export const NO_EDITABEL_COLS = ["_id", "id", "username"];
--- client-react-iso/src/containers/Admin/AdminMenu/MenuAddForm.js
+++ client-react-iso/src/containers/Admin/AdminMenu/MenuAddForm.js
@@ -1,38 +1,103 @@
-import React from "react";
-import { Row, Col, Button, Table, Modal, Input } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import React, { useState, useEffect } from "react";
+import { Modal, Input, Form } from "antd";
+import { UserOutlined, TagOutlined, MenuOutlined, FileTextOutlined } from "@ant-design/icons";
+import { createMenuApi, updateMenuApi } from "@redux/adminUsers/actions";
+import { useDispatch, useSelector } from "react-redux";
 
-export default function MenuAddForm({ handleOk, handleCancel, isModalVisible, ...props }) {
+export default function MenuAddForm({ modalMode, initialValues, handleCancel, isModalVisible, setIsModalVisible, ...props }) {
+  const [form] = Form.useForm();
+  const token = useSelector((state) => state.Auth.idToken);
+
+  const dispatch = useDispatch();
+
+  const handleOk = async () => {
+    try {
+      const values = await form.validateFields();
+      console.log("Success:", values);
+      // TODO: Send dữ liệu về server và thông báo kết quả
+      // Nếu có initialValues tức là đang edit thì gọi hàm edit chứ đừng dại gọi add hì
+      if (initialValues && modalMode === "EDIT") {
+        dispatch(updateMenuApi(token, form.getFieldValue()));
+        setIsModalVisible(false);
+        return;
+      }
+      dispatch(createMenuApi(token, form.getFieldValue()));
+      setIsModalVisible(false);
+    } catch (errorInfo) {
+      console.log("Failed:", errorInfo);
+    }
+  };
+
+  useEffect(() => {
+    if (modalMode === "ADD") {
+      form.resetFields();
+      return;
+    }
+    if (initialValues && modalMode === "EDIT") {
+      form.setFieldsValue({
+        ...initialValues,
+      });
+    }
+  }, [initialValues, modalMode]);
+
+  const formItemLayout = {
+    labelCol: { xs: { span: 24 }, sm: { span: 5 } },
+    wrapperCol: { xs: { span: 24 }, sm: { span: 20 } },
+  };
+
   return (
-    <Modal {...props} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-      <Input name="tag_id" size="large" placeholder="Nhập Tag ID" prefix={<UserOutlined />} onKeyUp={props.handleFormChange} />
-      <br />
-      <br />
-      <Input
-        name="page"
-        size="large"
-        placeholder="Nhập Page (Route URL)"
-        prefix={<UserOutlined />}
-        onKeyUp={props.handleFormChange}
-      />
-      <br />
-      <br />
-      <Input
-        name="name"
-        size="large"
-        placeholder="Nhập Tên Hiển Thị"
-        prefix={<UserOutlined />}
-        onKeyUp={props.handleFormChange}
-      />
-      <br />
-      <br />
-      <Input
-        name="description"
-        size="large"
-        placeholder="Nhập Mô Tả Cho Route"
-        prefix={<UserOutlined />}
-        onKeyUp={props.handleFormChange}
-      />
+    <Modal
+      {...props}
+      // cancelButtonProps={{ block: true }}
+      // okButtonProps={{ block: true }}
+      visible={isModalVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Form {...formItemLayout} form={form}>
+        <Form.Item
+          label="Tag ID"
+          name="tag_id"
+          rules={[
+            {
+              required: true,
+              message: "Bạn phải nhập tag id",
+            },
+          ]}
+        >
+          <Input size="large" placeholder="Nhập Tag ID" prefix={<TagOutlined />} />
+        </Form.Item>
+        <Form.Item label="App URL" name="page">
+          <Input size="large" placeholder="Nhập Page (Route URL)" prefix={<MenuOutlined />} />
+        </Form.Item>
+        <Form.Item
+          label="Tên"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: "Bạn phải nhập tên hiển thị",
+            },
+          ]}
+        >
+          <Input size="large" placeholder="Nhập Tên Hiển Thị" prefix={<UserOutlined />} />
+        </Form.Item>
+        <Form.Item
+          label="Mô Tả"
+          name="description"
+          rules={[
+            {
+              required: true,
+              message: "Bạn phải nhập mô tả cho route này",
+            },
+          ]}
+        >
+          <Input.TextArea size="large" placeholder="Nhập Mô Tả Cho Route" prefix={<FileTextOutlined />} />
+        </Form.Item>
+        <p style={{ fontSize: 10, color: "grey" }}>
+          <span style={{ color: "red" }}>*</span> Trường bắt buộc nhập liệu
+        </p>
+      </Form>
     </Modal>
   );
 }
