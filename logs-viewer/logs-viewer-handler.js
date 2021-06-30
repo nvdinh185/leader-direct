927bc0e5ccc8126b644f7e43399d77b4c876ebb6
hieu.quang <hieu.quang@mobifone.vn>
2021-06-30 14:26:28 +0700

    rev after merge fix logs json model err

--- logs-viewer/logs-viewer-handler.js
+++
@@ -1,5 +0,0 @@
-// nhúng router để gán hàm 
-module.exports = (req, res, next) => {
-  req.logAccess = require("./logs-viewer-save");
-  next();
-};
--- midlewares/logs/config/index.js
+++
@@ -1,3 +0,0 @@
-// Đây là thủ tục tạo tự động từ ./test/create-models by cuong.dq
-
-module.exports = require("../../../db/db-connection-pool-logs");
